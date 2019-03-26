64ec71c490f1e427a4024c5c1b0eecd4fb772a0c
Bwitz <guitarrook94@gmail.com>
2019-03-26 06:32:00 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import Homepage from './Web-Pages/Homepage';
 import './App.css';
 
 class App extends Component {
 
+  state = {
+    currentPage: "Homepage"
+  }
+
+  webpageHandler = () => {
+    switch(this.state.currentPage) {
+      case "Homepage":
+        return <Homepage />
+      default:
+        return <Homepage />
+    }
+  }
+
   render() {
     return (
       <div>
-        <Homepage />
+        <Switch>
+          <Route path="Home" component={Homepage} />
+        </Switch>
+        {this.webpageHandler()}
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/Web-Pages/Homepage.js
+++ src/Web-Pages/Homepage.js
@@ -2,5 +2,14 @@ import React, { Component } from 'react';
 import "../App.css";
 
 export default class Homepage extends Component {
-    
+
+    render() {
+
+        return (
+            <div>
+                <p>Test!</p>
+            </div>
+        )
+
+    }
 }
